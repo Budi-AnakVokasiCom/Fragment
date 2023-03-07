d58aa4caf818089065f55c37fc6bb6c025d17867
Budi-AnakVokasiCom <budisugiarto@mail.ugm.ac.id>
2023-03-07 22:30:58 +0700

    commit dinamic

--- app/src/main/java/com/example/fragment/DinamisFragment.java
+++ app/src/main/java/com/example/fragment/DinamisFragment.java
@@ -8,25 +8,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SimpleFragment} factory method to
- * create an instance of this fragment.
- */
-public class SimpleFragment extends Fragment {
 
-    public SimpleFragment() {
+public class DinamisFragment extends Fragment {
+
+    public DinamisFragment() {
         // Required empty public constructor
     }
 
-    public static SimpleFragment newInstance() {
-        return new SimpleFragment();
+    public static DinamisFragment newInstance() {
+        return new DinamisFragment();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_simple, container, false);
+        return inflater.inflate(R.layout.fragment_dinamis, container, false);
     }
 }
